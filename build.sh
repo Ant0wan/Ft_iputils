6e9f7f14ec254181a58b7b1a4449d79846adc4ee
Antoine Barthelemy <antoine@linux.com>
2023-01-10 19:56:55 +0100

    test getopt

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 [ ! -x ping ]
 [ -x configure ]
 ./configure
-make -j
-sudo setcap cap_net_admin,cap_net_raw=eip ping
+sudo make -j
+#sudo setcap cap_net_admin,cap_net_raw=eip ping
 
 #rm -rf tags *.o *.d ping *.log *.status *.conf autom4te.cache/
