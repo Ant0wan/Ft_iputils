9cd8d6e300db6f2c42b6c8d69e54d613e294debc
Antoine Barthelemy <antoine@linux.com>
2023-04-16 23:47:06 +0200

    update

--- ping.c
+++ ping.c
@@ -1,53 +1,50 @@
-#include <unistd.h>
 #include <locale.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "iputils_common.h"
 
-//int main(int argc, char **argv)
+// int main(int argc, char **argv)
 //{
 //	atexit(print);
 //	setlocale(LC_ALL, "");
 //	return 0;
-//}
+// }
 
+int main(int argc, char **argv) {
+  int flags, opt;
+  int nsecs, tfnd;
 
-int main(int argc, char **argv)
-{
-	int flags, opt;
-	int nsecs, tfnd;
+  nsecs = 0;
+  tfnd = 0;
+  flags = 0;
+  while ((opt = getopt(argc, argv, "nt:")) != -1) {
+    switch (opt) {
+    case 'n':
+      flags = 1;
+      break;
+    case 't':
+      nsecs = atoi(optarg);
+      tfnd = 1;
+      break;
+    default: /* '?' */
+      fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
 
-	nsecs = 0;
-	tfnd = 0;
-	flags = 0;
-	while ((opt = getopt(argc, argv, "nt:")) != -1) {
-		switch (opt) {
-			case 'n':
-				flags = 1;
-				break;
-			case 't':
-				nsecs = atoi(optarg);
-				tfnd = 1;
-				break;
-			default: /* '?' */
-				fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-						argv[0]);
-				exit(EXIT_FAILURE);
-		}
-	}
+  printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n", flags, tfnd, nsecs,
+         optind);
 
-	printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n",
-			flags, tfnd, nsecs, optind);
+  if (optind >= argc) {
+    fprintf(stderr, "Expected argument after options\n");
+    exit(EXIT_FAILURE);
+  }
 
-	if (optind >= argc) {
-		fprintf(stderr, "Expected argument after options\n");
-		exit(EXIT_FAILURE);
-	}
+  printf("name argument = %s\n", argv[optind]);
 
-	printf("name argument = %s\n", argv[optind]);
+  /* Other code omitted */
 
-	/* Other code omitted */
-
-	exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
